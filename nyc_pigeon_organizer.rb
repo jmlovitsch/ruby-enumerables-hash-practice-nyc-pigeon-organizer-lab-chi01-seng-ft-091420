238020e31cc4d9ebfdac34f6e7215f5b9c04d778
John Lovitsch <jmlovitsch@gmail.com>
2020-09-03 14:32:23 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,5 +4,7 @@ def nyc_pigeon_organizer(data)
   data.each_with_object({pigeon_list}) do |(key, value) output_array|
     value.each do |inner_key, names|
       names.each do |name|
+    end
   end
+output_array
 end
