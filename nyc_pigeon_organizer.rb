29e5030315046b271eb8eed555a43fa5cd5de023
John Lovitsch <jmlovitsch@gmail.com>
2020-09-03 14:30:16 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,8 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
+  data.each_with_object({pigeon_list}) do |(key, value) output_array|
+    binding.pry
+    output_array
+  end
 end
